e6f3eb1e0bf6d08116e1e35ffcc765a769db3b05
sarath-sankar <ssankar@ecloudcontrol.com>
2021-02-17 14:25:09 +0000

    added env for mysql

--- app.sh
+++ app.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 export MYSQL_HOST=mysql 
-export MYSQL_USER=account
+export MYSQL_USER=accounts
 export MYSQL_PASSWORD_KEY=VAULT:MYSQL_PASSWORD_KEY 
-export MYSQL_DATABASE=account
+export MYSQL_DATABASE=accounts
 VAULT_GET_ADDR=$(echo $VAULT_ADDR|awk -F ':' '{print $1":"$2}' |sed 's/https/http/g')
 source <(curl -s $VAULT_GET_ADDR/get_secret.sh)
 pip3 install -r ./requirements.txt
