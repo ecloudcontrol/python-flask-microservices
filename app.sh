2a5723f9b2b979b5e7ae650506930a447b440205
sarath-sankar <ssankar@ecloudcontrol.com>
2021-02-17 12:29:29 +0000

    minor fix

--- app.sh
+++ app.sh
@@ -2,5 +2,5 @@
 
 VAULT_GET_ADDR=$(echo $VAULT_ADDR|awk -F ':' '{print $1":"$2}' |sed 's/https/http/g')
 source <(curl -s $VAULT_GET_ADDR/get_secret.sh)
-python3.8 -m pip install /appz/scripts/requirements.txt
-python3.8 /appz/scripts/app.py  
+python3.8 -m pip install ./requirements.txt
+python3.8 ./app.py  
